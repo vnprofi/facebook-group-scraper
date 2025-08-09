88f55b617b7ccb8f55aecfafdc21d95ffb05e6f8
vnprofi <vn3608950@gmail.com>
2025-08-09 21:50:20 +0300

    Create preload.js

---
+++ src /preload.js
@@ -0,0 +1,11 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  saveCSV: (data) => ipcRenderer.invoke('save-csv', data),
+  getScript: () => ipcRenderer.invoke('get-script'),
+  openExternal: (url) => ipcRenderer.invoke('open-external', url),
+  onScriptReady: () => {
+    // Уведомляем renderer, что скрипт готов
+    window.postMessage({ type: 'SCRIPT_READY' }, '*');
+  }
+});
